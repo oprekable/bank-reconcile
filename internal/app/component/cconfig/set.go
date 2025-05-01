28630385093d6fd5cf85bdf9b490e27bfc10caf2
Eko Fedriyanto <eko@firstlabstudio.com>
2025-05-01 22:31:52 +0700

    remove unnecessary wire argument

--- internal/app/component/cconfig/set.go
+++ internal/app/component/cconfig/set.go
@@ -2,12 +2,8 @@ package cconfig
 
 import (
 	"github.com/google/wire"
-	"github.com/spf13/afero"
 )
 
-type ConfigFS afero.Fs
-
 var Set = wire.NewSet(
-	wire.InterfaceValue(new(ConfigFS), afero.NewOsFs()),
 	NewConfig,
 )
